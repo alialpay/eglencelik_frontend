248acfae4aa447d08135dd3be6edff3e58aaa9df
Ali Alpay <alialpay96@hotmail.com>
2022-06-19 20:51:33 +0300

    signupPage

--- src/UserSignupPage.js
+++ src/UserSignupPage.js
@@ -1,6 +1,42 @@
 import React from "react";
 
 class UserSignupPage extends React.Component{
+    state = {
+        username: null,
+        displayName: null,
+        password: null,
+        passwordRepeat: null
+    };
+
+    onChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        });
+    };
+    // onChangeUsername = event => {
+    //     this.setState({
+    //         username: event.target.value
+    //     })
+    // };
+
+    // onChangeDisplayName = event => {
+    //     this.setState({
+    //         displayName: event.target.value
+    //     })
+    // };
+
+    // onChangePassword = event => {
+    //     this.setState({
+    //         password: event.target.value
+    //     })
+    // };
+
+    // onChangePasswordRepeat = event => {
+    //     this.setState({
+    //         passwordRepeat: event.target.value
+    //     })
+    // };
 
     render() {
         return (
@@ -8,19 +44,19 @@ class UserSignupPage extends React.Component{
                 <h1>Sign Up</h1>
                 <div>
                     <label>Username</label>
-                    <input/>
+                    <input name="username" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input/>
+                    <input name="displayName" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" />
+                    <input name="password" type="password" onChange={this.onChange} />
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password" />
+                    <input name="passwordRepeat" type="password" onChange={this.onChange} />
                 </div>
                 <button>Sign Up</button>
             </form>
